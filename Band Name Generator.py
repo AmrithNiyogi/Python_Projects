6b9c5e1f0953e9bb8b989baeb3b5dcc23e64a8a9
AmrithNiyogi <niyogiamrith@gmail.com>
2023-06-20 11:09:00 +0530

    Create Band Name Generator.py

---
+++ Band Name Generator.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 19 19:15:19 2023
+
+@author: amrit
+"""
+
+print('Welcome to the Band Name Generator\n')
+
+city_name = input('Enter the name of your city: \n')
+
+pet_name = input("Enter the name of your pet: \n")
+
+band_name = city_name + " " + pet_name
+
+print(f'The name of your band is: {band_name}')
